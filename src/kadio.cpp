ee7998ca2edef596d236700343a75235345e3146
jsc <jcruz@posteo.net>
2023-03-23 18:18:26 +0000

    Clean-up widget creation

--- src/kadio.cpp
+++ src/kadio.cpp
@@ -16,14 +16,13 @@ kadio::kadio(const QVector<QString>& words, QWidget *parent) :
     QWidget* window = new QWidget(this);
     QHBoxLayout* main_layout = new QHBoxLayout(window);
 
-    QWidget* left_pane = new QWidget(this);
+    QWidget* left_pane = new QWidget;
+    main_layout->addWidget(left_pane);
     QVBoxLayout* left_pane_layout = new QVBoxLayout(left_pane);
 
     for (const QString& w : words) {
         left_pane_layout->addWidget(new QLabel(w));
     }
-    main_layout->addWidget(left_pane);
-
 
     QLabel* right_pane = new QLabel("four");
     main_layout->addWidget(right_pane);
